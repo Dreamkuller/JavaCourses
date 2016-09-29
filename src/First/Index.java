5e7c362b1c7bbfb6ef940591ed7d7760f19b25ec
Dreamkuller <andrewyo44@gmail.com>
2016-09-29 16:47:42 +0300

    Homework 8

---
+++ src/First/Index.java
@@ -0,0 +1,17 @@
+package First;
+
+/**
+ * Created by Andrew on 20.09.2016.
+ */
+public class Index {
+    public static void main(String[] args) {
+        int a = 5;
+        int c = a + 1;
+        int b = a + c;
+        System.out.println("a+b="+b);
+
+
+    }
+
+
+}
